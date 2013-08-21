158f0c92c60bc2eb8b236de577ec30ef08912aec
mrbendel <doniguan@gmail.com>
2013-08-21 17:23:21 -0400

    removed ref to imageview+af because we shouldn't be using it

--- AFNetworking/AFNetworking.h
+++ AFNetworking/AFNetworking.h
@@ -38,7 +38,5 @@
 
     #if defined(__IPHONE_OS_VERSION_MIN_REQUIRED)
         #import "AFNetworkActivityIndicatorManager.h"
-        #import "UIImageView+AFNetworking.h"
-        #import "UIButton+AFNetworking.h"
     #endif
 #endif /* _AFNETWORKING_ */
